a329317acc3a00dfc8f00c7c2ab84a6165eb52b6
molinero95 <jaimolas95@gmail.com>
2017-12-20 19:23:43 +0100

    arreglo

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Meter en un JSON
 function getDistricts(){
     let distritos = [];
     let chamberi = [
@@ -45,10 +46,47 @@ function getDistricts(){
         {lat: 40.474449, lng: -3.6909414}, //Sinesio Delgado 3 carriles
         {lat: 40.4746582, lng: -3.6892631}, //Sinesio Delgado rotonda
         {lat: 40.4741122, lng: -3.6870455}, //Sinesio Delgado con Castellana
-        {lat: 40.4659765, lng: -3.6893288},
+        {lat: 40.4659765, lng: -3.6893288}, //Plaza de Castilla
         {lat: 40.4462073, lng: -3.6912829}, //Nuevos Ministerios
     ];
     distritos.push(nuevosMinisterios);
+    //Terminar
+    let manoteras = [
+        {lat: 40.48394, lng: -3.6733336}, //M11 con A1
+        {lat: 40.4909269, lng: -3.6717726}, //A1
+        {lat: 40.5034809, lng: -3.6579914}, //A1 via de servicio
+        //Seguir por aqui
+        {lat: 40.5091634, lng: -3.6254723}, //Curva colegio
+        {lat: 40.5085406, lng: -3.6230203}, //Camino viejo del cura
+        {lat: 40.5098617, lng: -3.6208385}, //Curva colegio
+        {lat: 40.5097576, lng: -3.6177968}, //Curva colegio
+        {lat: 40.5107299, lng: -3.6154616}, //Curva colegio
+        {lat: 40.510477, lng: -3.6139978}, //Colegio Escandinavo
+        {lat: 40.5012218, lng: -3.6016404}, //M12
+        {lat: 40.4724417, lng: -3.6012862}, //Elipa
+        {lat: 40.4700992, lng: -3.6024925}, //Ariadna/Elipa 
+        {lat: 40.4713994, lng: -3.6066256}, //Calle Ariadna
+        {lat: 40.4722702, lng: -3.6232352}, //M11
+        {lat: 40.470457, lng: -3.6245469}, //Via Dublín
+        {lat: 40.468595, lng: -3.6242277}, //IFEMA
+        {lat: 40.4683971, lng: -3.6275209}, //M40 entrada 5
+        {lat: 40.4604459, lng: -3.6213954}, //M40 salida 6
+        {lat: 40.4550925, lng: -3.6120607}, //M40
+        {lat: 40.4527905, lng: -3.6053651}, //M40
+        {lat: 40.4533948, lng: -3.6045876}, //Osuna Camping
+        {lat: 40.4496326, lng: -3.6038393}, //A2 Av América
+        {lat: 40.4492287, lng: -3.6305219}, //A2
+        {lat: 40.4484935, lng: -3.6366989}, //A2
+        {lat: 40.4497676, lng: -3.6458168}, //A2
+        {lat: 40.4493461, lng: -3.6480506}, //Asuna con A2
+        {lat: 40.4593258, lng: -3.6562734}, //Asuna
+        {lat: 40.4633048, lng: -3.6620066}, //Mesena
+        {lat: 40.4764452, lng: -3.6657567}, //Golfo de Salónica
+        {lat: 40.4776936, lng: -3.665413}, //Golfo de Salónica
+        {lat: 40.4830271, lng: -3.6600191}, //Manuel Azaña
+        {lat: 40.48394, lng: -3.6733336}, //M11 con A1
+    ];
+    distritos.push(manoteras);
     console.log(distritos);
     return distritos;
 }
@@ -68,7 +106,7 @@ function initMap() {
         strokeColor: '#FF0000',
         strokeOpacity: 1.0,
         strokeWeight: 2
-    });
+    }).setMap(map);
     let tetuanPinta = new google.maps.Polygon({
         path: distritos[1],
         geodesic: true,
@@ -76,8 +114,15 @@ function initMap() {
         strokeColor: '#03AEB2',
         strokeOpacity: 1.0,
         strokeWeight: 2
-    })
-    chamberiPinta.setMap(map);
-    tetuanPinta.setMap(map);
+    }).setMap(map);
+    let manoteras = new google.maps.Polygon({
+        path: distritos[2],
+        geodesic: true,
+        fillColor: '#03AEB2',
+        strokeColor: '#03AEB2',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    }).setMap(map);
+
 }
 
