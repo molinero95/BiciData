303129aa7b4512acdffe54dcb3ed04f46d1f411f
marios1294 <34682570+marios1294@users.noreply.github.com>
2018-01-14 00:59:37 +0100

    reducerHoraDist.py

--- Python/modificaciones/reducerHoraDist.py
+++ Python/modificaciones/reducerHoraDist.py
@@ -6,18 +6,22 @@ suma = 0
 print 
 for line in sorted(sys.stdin):
 	dist, hora = line.split( '\t' )
-	if dist != prev:
-		print dist + '\n'
-		prev = dist
+	
 	if  hora!= previous:
-		print '%s\t%s' % (suma, hora)
+		print '%s\t%s\t%s' % (dist,suma, previous)
 		
 		
 		previous = hora
 		suma = 0
 	suma = suma + 1
 	
+
 	
+	
+		
+	
+
+
 	
 	
 		
